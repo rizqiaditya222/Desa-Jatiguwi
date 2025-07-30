195fb84e90ad30e63da6071186e763ae6825f322
Biz <bisma1002@gmail.com>
2025-07-30 08:46:53 +0700

    perbaikan kecil
    sori lupa apus sesuatu

--- src/app/selayang-pandang/liststatcard.tsx
+++ src/app/selayang-pandang/liststatcard.tsx
@@ -6,7 +6,7 @@ interface ListStatCardProps {
 
 const ListStatCard: React.FC<ListStatCardProps> = ({ title, items }) => {
   return (
-    <div className="w-full flex flex-col items-center p-8 border border-[#0E4D45] bg-gray-200 rounded-xl text-[#0E4D45] shadow-md">
+    <div className="w-full flex flex-col items-center p-8 border-[#0E4D45] bg-gray-200 rounded-xl text-[#0E4D45] shadow-md">
       <h1 className="mb-6 text-3xl font-semibold">{title}</h1>
       <div className="w-full max-w-2xl space-y-4">
         {items.map((item, index) => (
